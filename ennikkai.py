b824cbb0c79fdfa3d434691cf542dbce108f6994
amotbeli <sedhupathy24@gmail.com>
2024-09-13 08:40:54 +0530

    Add comments for better readability of the code

--- ennikkai.py
+++ ennikkai.py
@@ -12,39 +12,50 @@ def main():
         file.write(output)
 
 def to_panmai(string):
-    string = tamil.utf8.get_letters(string)
-    last_letter_split = tamil.utf8.splitMeiUyir(string[-1])
-    first_letter_split = tamil.utf8.splitMeiUyir(string[-2])
+    string_list = tamil.utf8.get_letters(string)
+    last_letter_split = tamil.utf8.splitMeiUyir(string_list[-1])
+    first_letter_split = tamil.utf8.splitMeiUyir(string_list[-2])
     nedils = tamil.utf8.nedil_letters
     kurils = tamil.utf8.kuril_letters
-    if string[-1] == "ம்":
-        string[-1] = "ங்"
-    elif string[-1] == "ல்" and len(string) == 2:
-        string[-1] = "ற்"
-    elif string[-1] == "ள்":
-        string[-1] = "ட்"
-    elif len(string) == 1:
-        string[-1] += "க்"
+    # படம்+கள் = படங்கள்
+    if string_list[-1] == "ம்":
+        string_list[-1] = "ங்"
+    # பல்+கள் = பற்கள் (but அணில்+கள் != அணிற்கள்)
+    elif string_list[-1] == "ல்" and len(string_list) == 2:
+        string_list[-1] = "ற்"
+    # முள்+கள் = முட்கள்
+    elif string_list[-1] == "ள்":
+        string_list[-1] = "ட்"
+    # பூ+கள் = பூக்கள்
+    elif len(string_list) == 1:
+        string_list[-1] += "க்"
+    # புறா+கள் = புறாக்கள் (but கலை+கள் != கலைக்கள்)
     elif last_letter_split[-1] in nedils and last_letter_split[-1] != "ஐ":
-        string[-1] += "க்"
-    elif len(string) == 2 and first_letter_split[-1] in kurils and last_letter_split[-1] in kurils:
-        string[-1] += "க்"
-    string = "".join(string)
+        string_list[-1] += "க்"
+    # பசு+கள் = பசுக்கள்
+    elif len(string_list) == 2 and first_letter_split[-1] in kurils and last_letter_split[-1] in kurils:
+        string_list[-1] += "க்"
+    string = "".join(string_list)
     output = string + "கள்"
     return output
 
 def to_orumai(string):
-    string = tamil.utf8.get_letters(string)
-    string = string[:-2]
-    if string[-1] == "ங்":
-        string[-1] = "ம்"
-    elif string[-1] == "ற்":
-        string[-1] = "ல்"
-    elif string[-1] == "ட்":
-        string[-1] = "ள்"
-    elif string[-1] == "க்":
-        string[-1] = ""
-    output = "".join(string)
+    string_list = tamil.utf8.get_letters(string)
+    # Remove the "கள்" விகுதி from plural words
+    string_list = string_list[:-2]
+    # (படங்கள்) படங் -> படம் 
+    if string_list[-1] == "ங்":
+        string_list[-1] = "ம்"
+    # (பற்கள்) பற் -> பல்
+    elif string_list[-1] == "ற்":
+        string_list[-1] = "ல்"
+    # (முட்கள்) முட் -> முள்
+    elif string_list[-1] == "ட்":
+        string_list[-1] = "ள்"
+    # (புறாக்கள்) புறாக் -> புறா
+    elif string_list[-1] == "க்":
+        string_list[-1] = ""
+    output = "".join(string_list)
     return output
 
 if __name__ == "__main__":
